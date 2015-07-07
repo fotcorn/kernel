38681f0378ca626696d18c2d7ea6d60b9bbcd6ef
fotcorn <fotcorn@gmail.com>
2015-07-07 15:25:17 +0200

    [Keyboard] disable PS/2 ports while initialising them

--- src/keyboard.c
+++ src/keyboard.c
@@ -1,8 +1,12 @@
 #include "keyboard.h"
 #include "screen.h"
 #include "pic.h"
+#include "io.h"
 
 void init_keyboard() {
+    outb(KEYBOARD_CMD, 0xAD); // disable first PS/2 port
+    outb(KEYBOARD_CMD, 0xA7); // disable second PS/2 port
+
     outb(KEYBOARD_CMD, 0x20); // read keyboard configuration
     while (!(inb(KEYBOARD_CMD) & 0x1)); // wait until data is in output buffer
     unsigned char config = inb(KEYBOARD_DATA); // read data
@@ -12,6 +16,9 @@ void init_keyboard() {
     outb(KEYBOARD_CMD, 0x60); // set keyboard configuration
     while ((inb(KEYBOARD_CMD) & 0x2)); // wait until data input buffer is clear
     outb(KEYBOARD_DATA, config); // write configuration
+
+    outb(KEYBOARD_CMD, 0xAE); // enable first PS/2 port
+    outb(KEYBOARD_CMD, 0xA8); // enable second PS/2 port
 }
 
 void keyboard_interrupt() {
